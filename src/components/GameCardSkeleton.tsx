3cc8a61be8114f4131b4421de5167b9d5e939251
Endri Simoni <endri.simoni1@gmail.com>
2024-02-12 10:35:50 +0100

    Fetch the genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="420px" borderRadius={10} overflow="hidden">
+    <Card>
       <Skeleton height="300px" />
       <CardBody>
         <SkeletonText />
