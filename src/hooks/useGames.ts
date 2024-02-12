e77d2767de6be9a3cd55489d7c45a7600640311e
Endri Simoni <endri.simoni1@gmail.com>
2024-02-12 08:11:55 +0100

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../service/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[]
 }
 
 interface FetchGameResponse {
@@ -24,9 +31,9 @@ const useGames = () => {
       .get<FetchGameResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
-        if(err instanceof CanceledError) return;
-        setError(err.message)
-    });
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
 
     return () => controller.abort();
   }, []);
